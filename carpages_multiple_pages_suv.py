cb8410816c915dec49a60d2b7c26c578bd987bdd
[JAXMES] <47646169+JAXMES@users.noreply.github.com>
2022-06-28 16:18:07 -0400

    Fixed counter and added car posting to its correct position

--- carpages_multiple_pages_suv.py
+++ carpages_multiple_pages_suv.py
@@ -14,38 +14,41 @@ soup = BeautifulSoup(page.text, 'lxml')
 
 soup
 
-#df = pd.DataFrame({'links':[''], 'Title':[''], 'Details':[''], 'Price':[''], 'Rating':['']})
+#Preparing Table
+df = pd.DataFrame({'links':[''], 'Name':[''], 'Price':[''], 'Color':['']})
 
-# Get postings link, name, price and color of each car
-# Only 10 pages and put all data into a table
 
-while True:
+# Only 5 pages and put all data into a table
+
+counter = 0
+while counter < 5:
     
+    # Get postings link, name, price and color of each car
     car_postings = soup.find_all('div', class_= 'l-column l-column--large-8') # Get all posts in the page
     car_postings
     
+    len(car_postings)
+    
     
      #TRY was added to catch any errors when we have null values. 
-    #For loop gives a EOF error which does not affect the loop... Do not run while True until complete and run the next pages code too
+    #For loop gives a EOF error which does not affect the loop... Careful with While function infinite loop
     for post in car_postings:
         try:
+            
             car_link = post.find('a').get('href')
             car_link_full = 'https://www.carpages.ca' + car_link
             
             car_name = post.find('a').text
             car_price = post.find('strong', class_= 'delta').text.strip()
             car_color = post.find_all('span')[-1].text.strip()
-            car_color
             
+            df = df.append({'links':car_link_full, 'Name':car_name, 'Price':car_price, 'Color':car_color}, ignore_index = True)
             
-            break
-            #df = df.append({'links':link_full, 'Title':title, 'Details':details, 'Price':price, 'Rating':rating}, ignore_index = True)
         except:
             pass
-      
+  
     
-    
-    next_page = soup.find('a', class_= 'nextprev').get('href') # URL subset
+    next_page = soup.find('a', class_= 'nextprev').get('href')# URL subset
     next_page
     next_page_full = 'https://www.carpages.ca/'+next_page
     next_page_full
@@ -53,3 +56,5 @@ while True:
     url = next_page_full
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'lxml')
+    
+    counter += 1
