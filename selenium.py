409dfc89773252c6d28b6ddec6318fe6527d7267
[JAXMES] <47646169+JAXMES@users.noreply.github.com>
2022-06-29 23:39:33 -0400

    Value Error fix in progress

---
+++ selenium.py
@@ -0,0 +1,10 @@
+# pip install webdriver-manager
+#pip install urllib3
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
