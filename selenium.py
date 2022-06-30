530e8c48e18933a73d8b0fbec680e8073af74896
[JAXMES] <47646169+JAXMES@users.noreply.github.com>
2022-06-30 14:53:30 -0400

    Google input & Keys was imported

--- selenium.py
+++ selenium.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 s=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
@@ -13,4 +14,15 @@ driver = webdriver.Chrome(service=s)
 
 driver.get("https://www.goat.com/sneakers")
 
-driver.find_element(By.XPATH, '/html/body/div[1]/div/main/div[1]/div/div[2]/div/div[1]/div[1]/a/div[1]/div[2]/div/div/span').text
+#Get all postings from 1-20 in the sneakers section
+for i in range(1, 20):
+    price = driver.find_element(By.XPATH, '/html/body/div[1]/div/main/div[1]/div/div[2]/div/div[1]/div['+str(i)+']/a/div[1]/div[2]/div/div/span').text
+    print(price)
+    
+# Google Input
+
+driver.get("https://www.google.com/")
+
+box = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+box.send_keys('Selenium')
+box.send_keys(Keys.RETURN)
