8e75c84a1af0d1bd497deee50f2f060171281631
[JAXMES] <47646169+JAXMES@users.noreply.github.com>
2022-06-30 11:11:11 -0400

    By import comples the old find_element_by_xpath method

--- selenium.py
+++ selenium.py
@@ -9,3 +9,8 @@ from selenium.webdriver.common.by import By
 
 s=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
+
+
+driver.get("https://www.goat.com/sneakers")
+
+driver.find_element(By.XPATH, '/html/body/div[1]/div/main/div[1]/div/div[2]/div/div[1]/div[1]/a/div[1]/div[2]/div/div/span').text
