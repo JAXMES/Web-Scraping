215d93e0ee65ea5debdbcaea779c4fee057233da
[JAXMES] <47646169+JAXMES@users.noreply.github.com>
2022-07-26 12:21:49 -0400

    Bsoup & loop to get product cards info

--- bsoup_selenium_nike_sales.py
+++ bsoup_selenium_nike_sales.py
@@ -24,7 +24,9 @@ driver = webdriver.Chrome(service=s)
 driver.get("https://www.nike.com/w/back-to-fall-4n8am")
 
 
+# Universal code to scroll until the end
 # Get the heigh of the page wait to load to compare to the new heigh and loop til we get to the end of the page
+
 last_height = driver.execute_script('return document.body.scrollHeight')
 
 while True:
@@ -35,3 +37,24 @@ while True:
     if new_height == last_height:
         break
     last_height = new_height
+    
+    
+#Initiating BSoup
+# We dont need to use request because we now have drive from selenium
+
+soup = BeautifulSoup(driver.page_source,'lxml')
+
+product_card = soup.find_all('div', class_= 'product-card__body')
+len(product_card)
+
+for product in product_card:
+    
+    link = product.find('a', class_= 'product-card__link-overlay').get('href')
+    name = product.find('div', class_= 'product-card__title').text
+    subtitle = product.find('div', class_= 'product-card__subtitle').text
+    full_price = product.find('div', class_= 'product-price is--striked-out css-0').text
+    sale_price = product.find('div', class_= 'product-price is--current-price css-1ydfahe').text
+    
+    break
+
+
