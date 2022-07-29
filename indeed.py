0650cb598f4a4ee952b897eedfc6a8e8764e6e3a
[JAXMES] <47646169+JAXMES@users.noreply.github.com>
2022-07-29 11:18:39 -0400

    Next page added, no errors & df contains all data

--- indeed.py
+++ indeed.py
@@ -26,7 +26,7 @@ time.sleep(3)
 #1. Input Job Title into input box
 
 box_job = driver.find_element(By.XPATH, "//input[@placeholder='Job title, keywords, or company']")
-box_job.send_keys("Data Analyst")
+box_job.send_keys("painting project manager")
 box_job.send_keys(Keys.RETURN)
 
 
@@ -38,26 +38,38 @@ len(postings)
 
 df= pd.DataFrame({'Link':[''], 'Job Title':[''], 'Company':[''], 'Location':[''], 'Salary':[''], 'Date':['']})
 
+while True:
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    postings = soup.find_all('div', class_= 'job_seen_beacon')
+    
+    
 #Loop to gather all the postings
-for post in postings:
-    link = post.find('a', class_= 'jcs-JobTitle css-jspxzf eu4oa1w0').get('href')
-    link_full = 'https://www.indeed.com/'+link
-    title = post.find('span').text
-    company = post.find('span', class_= 'companyName').text
+    for post in postings:
+        link = post.find('a', class_= 'jcs-JobTitle css-jspxzf eu4oa1w0').get('href')
+        link_full = 'https://www.indeed.com/'+link
+        title = post.find('span').text
+        company = post.find('span', class_= 'companyName').text
+        try:
+            location = post.find('div', class_= 'companyLocation').text.strip()
+        except:
+            location = 'N/A'
+        # Remove extra hidden content in date
+        try:
+            post.find('span', class_= 'visually-hidden').decompose()
+            date = post.find('span', class_= 'date').text
+        except:
+            date = 'N/A'
+        try:
+            salary = post.find('div', class_= 'attribute_snippet').text
+        except:
+            salary = 'N/A'
+        df = df.append({'Link':link_full, 'Job Title':title, 'Company':company, 'Location':location, 'Salary':salary, 'Date':date}, 
+                        ignore_index = True)
+        
     try:
-        location = post.find('div', class_= 'companyLocation').text.strip()
+        button = soup.find('a', attrs = {'aria-label': 'Next'}).get('href')
+        driver.get('https://www.indeed.com/'+button)
     except:
-        location = 'N/A'
-    # Remove extra hidden content in date
-    try:
-        post.find('span', class_= 'visually-hidden').decompose()
-        date = post.find('span', class_= 'date').text
-    except:
-        date = 'N/A'
-    try:
-        salary = post.find('div', class_= 'attribute_snippet').text
-    except:
-        salary = 'N/A'
-    df = df.append({'Link':link_full, 'Job Title':title, 'Company':company, 'Location':location, 'Salary':salary, 'Date':date}, 
-                    ignore_index = True)
+        break
 
