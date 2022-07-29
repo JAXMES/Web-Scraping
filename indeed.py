6385b5abf18b18224d61c855845010020931fe56
[JAXMES] <47646169+JAXMES@users.noreply.github.com>
2022-07-29 10:14:26 -0400

    Adding columns info for df

--- indeed.py
+++ indeed.py
@@ -28,3 +28,24 @@ time.sleep(3)
 box_job = driver.find_element(By.XPATH, "//input[@placeholder='Job title, keywords, or company']")
 box_job.send_keys("Data Analyst")
 box_job.send_keys(Keys.RETURN)
+
+
+#2. Get Link, Title, Company, Salary, Date & Location
+#Bsoup
+soup = BeautifulSoup(driver.page_source, 'lxml')
+postings = soup.find_all('div', class_= 'job_seen_beacon')
+len(postings)
+
+#Loop to gather all the postings
+for post in postings:
+    link = post.find('a', class_= 'jcs-JobTitle css-jspxzf eu4oa1w0').get('href')
+    link_full = 'https://www.indeed.com/'+link
+    title = post.find('span').text
+    company = post.find('span', class_= 'companyName').text
+    try:
+        location = post.find('div', class_= 'companyLocation').text.strip()
+    except:
+        location = 'N/A'
+    post.find('span', class_= 'visually-hidden').decompose()
+    date = post.find('span', class_= 'date')
+    date
