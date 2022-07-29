d79d596520553ec3030faa31fd0888f0b8d0afdf
[JAXMES] <47646169+JAXMES@users.noreply.github.com>
2022-07-28 23:29:18 -0400

    Initiating indeed project

---
+++ indeed.py
@@ -0,0 +1,25 @@
+# pip install webdriver-manager
+# pip install urllib3
+# Admin permissions are needed in windows
+
+from bs4 import BeautifulSoup
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+s=Service(ChromeDriverManager().install())
+
+# Starts the browser
+driver = webdriver.Chrome(service=s)
+
+driver.get("https://www.indeed.com/")
+time.sleep(3)
+
